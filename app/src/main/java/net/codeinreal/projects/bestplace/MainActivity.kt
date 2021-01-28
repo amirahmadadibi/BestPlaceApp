0cfc97781633d76a82fd42815fcc84ed1bf8165d
amirahmadadibi <amirahmadadibi@gmail.com>
2021-01-28 16:05:10 +0330

    the very basic and raw project

---
+++ app/src/main/java/net/codeinreal/projects/bestplace/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.codeinreal.projects.bestplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
