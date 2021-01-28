35425489efe78c82aea697df6ead55cee17d482d
amirahmadadibi <amirahmadadibi@gmail.com>
2021-01-28 17:21:03 +0330

    E01-finalCode

---
+++ app/src/main/java/net/codeinreal/projects/bestplace/AddPlaceActivity.kt
@@ -0,0 +1,11 @@
+package net.codeinreal.projects.bestplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_place)
+    }
+}
