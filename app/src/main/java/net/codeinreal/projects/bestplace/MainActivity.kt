bccad0cc9881fa7f2004fa22a470b179930e439f
amirahmadadibi <amirahmadadibi@gmail.com>
2021-01-28 18:30:47 +0330

    E02-addViewBinding

--- app/src/main/java/net/codeinreal/projects/bestplace/MainActivity.kt
+++ app/src/main/java/net/codeinreal/projects/bestplace/MainActivity.kt
@@ -1,12 +1,20 @@
 package net.codeinreal.projects.bestplace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import net.codeinreal.projects.bestplace.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this@MainActivity))
+        setContentView(binding.root)
 
+        binding.fabAddPlaceActivity.setOnClickListener {
+            val intentToAddPlaceActivity = Intent(this@MainActivity,AddPlaceActivity::class.java)
+            startActivity(intentToAddPlaceActivity)
+        }
     }
 }
