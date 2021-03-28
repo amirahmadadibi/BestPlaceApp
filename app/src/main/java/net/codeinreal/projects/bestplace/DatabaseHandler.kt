26ef0ffe9384256df88602cfca7827e79ca788d7
amirahmadadibi <amirahmadadibi@gmail.com>
2021-03-28 17:49:18 +0430

    E13-readingFromDatabase

--- app/src/main/java/net/codeinreal/projects/bestplace/DatabaseHandler.kt
+++ app/src/main/java/net/codeinreal/projects/bestplace/DatabaseHandler.kt
@@ -3,12 +3,12 @@ package net.codeinreal.projects.bestplace
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.constraintlayout.motion.widget.KeyTimeCycle
 
-class DatabaseHandler(context: Context)
-    :SQLiteOpenHelper(context,"bestplace",null,2){
-    companion object{
+class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, "bestplace", null, 2) {
+    companion object {
         const val TABLE_NAME = "bestplacetable"
         const val KEY_ID = "KEY_ID"
         const val KEY_TITLE = "_title"
@@ -21,17 +21,16 @@ class DatabaseHandler(context: Context)
     }
 
 
-
     override fun onCreate(db: SQLiteDatabase?) {
-         val createTableBestPlace = ("create table " + TABLE_NAME + " ( "
-                 + KEY_ID + " text primary key, "
-                 + KEY_TITLE + " text, "
-                 + KEY_IMAGE + " text, "
-                 + KEY_DESCRIPTION + " text, "
-                 + KEY_DATE + " text, "
-                 + KEY_LOCATION + " text, "
-                 + KEY_LATITUDE + " text, "
-                 + KEY_LONGITUDE + " text )")
+        val createTableBestPlace = ("create table " + TABLE_NAME + " ( "
+                + KEY_ID + " text primary key, "
+                + KEY_TITLE + " text, "
+                + KEY_IMAGE + " text, "
+                + KEY_DESCRIPTION + " text, "
+                + KEY_DATE + " text, "
+                + KEY_LOCATION + " text, "
+                + KEY_LATITUDE + " text, "
+                + KEY_LONGITUDE + " text )")
         db?.execSQL(createTableBestPlace)
     }
 
@@ -40,19 +39,50 @@ class DatabaseHandler(context: Context)
         onCreate(db)
     }
 
-    fun addPlace(bestPlace:BestPlace){
+    fun addPlace(bestPlace: BestPlace) {
         val writableDatabase = this.writableDatabase//getWritableDatabase()
         val contentValues = ContentValues()
-        contentValues.put(KEY_ID,bestPlace.id)
-        contentValues.put(KEY_TITLE,bestPlace.title)
-        contentValues.put(KEY_IMAGE,bestPlace.image)
-        contentValues.put(KEY_DESCRIPTION,bestPlace.description)
-        contentValues.put(KEY_DATE,bestPlace.date)
-        contentValues.put(KEY_LOCATION,bestPlace.location)
-        contentValues.put(KEY_LATITUDE,bestPlace.latitude)
-        contentValues.put(KEY_LONGITUDE,bestPlace.longitude)
+        contentValues.put(KEY_ID, bestPlace.id)
+        contentValues.put(KEY_TITLE, bestPlace.title)
+        contentValues.put(KEY_IMAGE, bestPlace.image)
+        contentValues.put(KEY_DESCRIPTION, bestPlace.description)
+        contentValues.put(KEY_DATE, bestPlace.date)
+        contentValues.put(KEY_LOCATION, bestPlace.location)
+        contentValues.put(KEY_LATITUDE, bestPlace.latitude)
+        contentValues.put(KEY_LONGITUDE, bestPlace.longitude)
 
-        writableDatabase.insert(TABLE_NAME,null,contentValues)
+        writableDatabase.insert(TABLE_NAME, null, contentValues)
         writableDatabase.close()
+    }
+
+
+    fun getAllPlaces(): ArrayList<BestPlace> {
+        val getAllQuery = "select * from ${TABLE_NAME}"//sql query
+        val db = this.readableDatabase
+        val bestplaceArray = ArrayList<BestPlace>()
+
+        try {
+            val cursor = db.rawQuery(getAllQuery, null)//result -> curosr
+            while (cursor.moveToNext()) {
+                val bestPlace = BestPlace(
+                    cursor.getString(cursor.getColumnIndex(KEY_TITLE)),
+                    cursor.getString(cursor.getColumnIndex(KEY_IMAGE)),
+                    cursor.getString(cursor.getColumnIndex(KEY_DESCRIPTION)),
+                    cursor.getString(cursor.getColumnIndex(KEY_DATE)),
+                    cursor.getString(cursor.getColumnIndex(KEY_LOCATION)),
+                    cursor.getString(cursor.getColumnIndex(KEY_LATITUDE)).toDouble(),
+                    cursor.getString(cursor.getColumnIndex(KEY_LONGITUDE)).toDouble()
+                )
+                bestPlace.id = cursor.getString(cursor.getColumnIndex(KEY_ID))
+
+                bestplaceArray.add(bestPlace)
+            }
+            if(!cursor.isClosed) cursor.close()
+        } catch (ex: SQLiteException) {
+            ex.printStackTrace()
+        }
+
+        db.close()
+        return  bestplaceArray
     }
 }
--- app/src/main/java/net/codeinreal/projects/bestplace/MainActivity.kt
+++ app/src/main/java/net/codeinreal/projects/bestplace/MainActivity.kt
@@ -3,6 +3,7 @@ package net.codeinreal.projects.bestplace
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import net.codeinreal.projects.bestplace.databinding.ActivityMainBinding
 
@@ -12,6 +13,13 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(LayoutInflater.from(this@MainActivity))
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        val db = DatabaseHandler(this)
+        val array  = db.getAllPlaces()
+        for (bestPlace in array) {
+            Log.d("tagx", "onCreate: ${bestPlace.id}")
+        }
+
         binding.fabAddPlaceActivity.setOnClickListener {
             val intentToAddPlaceActivity = Intent(this@MainActivity,AddPlaceActivity::class.java)
             startActivity(intentToAddPlaceActivity)
