92e1e1b63b64230a439ed5b573dfd4b2d78dd0aa
amirahmadadibi <amirahmadadibi@gmail.com>
2021-06-28 00:24:30 +0430

    E27-putSelectedStyleOnTheMap

--- app/src/main/java/net/codeinreal/projects/bestplace/SelectPlaceActivity.kt
+++ app/src/main/java/net/codeinreal/projects/bestplace/SelectPlaceActivity.kt
@@ -3,18 +3,30 @@ package net.codeinreal.projects.bestplace
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.style.layers.Layer
+import com.mapbox.mapboxsdk.style.layers.Property
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import net.codeinreal.projects.bestplace.databinding.ActivitySelectPlaceBinding
-
+import net.codeinreal.projects.bestplace.R
 
 class SelectPlaceActivity : AppCompatActivity() {
     lateinit var binding: ActivitySelectPlaceBinding
     private var hoveringMarker: ImageView? = null
+    private var droppedMarkerLayer:Layer? = null
+    companion object {
+        const val DROPPED_MARKER_LAYER_ID = "DROPPED_MARKER_LAYER_ID"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,19 +40,65 @@ class SelectPlaceActivity : AppCompatActivity() {
         binding.mapView.onCreate(savedInstanceState)
         binding.mapView.getMapAsync { mapBoxMap ->
             mapBoxMap.setStyle(Style.MAPBOX_STREETS) { style ->
-                hoveringMarker = ImageView(this@SelectPlaceActivity)
-                hoveringMarker!!.setBackgroundResource(R.drawable.ic_marker_red)
+                showRedPickerImage()
+                creteGreenSelectedMark(style)
+                binding.buttonSelectCurrentPlace.setOnClickListener {
+                    val mapTargetLatLan = mapBoxMap!!.cameraPosition.target
+                    hoveringMarker!!.visibility = View.INVISIBLE
 
-                val imageLocation = FrameLayout.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    Gravity.CENTER
-                )
+                    binding.buttonSelectCurrentPlace.setBackgroundColor(
+                        ContextCompat.getColor(this,R.color.mapbox_blue)!!)
 
-                hoveringMarker!!.layoutParams = imageLocation
+                    binding.buttonSelectCurrentPlace.setText("موقعیت انتخاب شد !")
 
-                binding.mapView.addView(hoveringMarker)
+                    val source = style.getSourceAs<GeoJsonSource>("dropped-icon-image")
+
+                    val targetPoint = Point.fromLngLat(mapTargetLatLan.longitude,mapTargetLatLan.latitude)
+
+                    source!!.setGeoJson(targetPoint)
+
+                    droppedMarkerLayer = style.getLayer(DROPPED_MARKER_LAYER_ID)
+                    droppedMarkerLayer!!.setProperties(PropertyFactory.visibility(Property.VISIBLE))
+                }
             }
         }
+    }
+
+    private fun showRedPickerImage() {
+        hoveringMarker = ImageView(this@SelectPlaceActivity)
+        hoveringMarker!!.setBackgroundResource(R.drawable.ic_marker_red)
+
+        val imageLocation = FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            Gravity.CENTER
+        )
+
+        hoveringMarker!!.layoutParams = imageLocation
+
+        binding.mapView.addView(hoveringMarker)
+    }
+
+
+    private fun creteGreenSelectedMark(loadedStyle: Style) {
+        loadedStyle.addImage(
+            "dropped-icon-image",
+            ContextCompat.getDrawable(this, R.drawable.ic_pin)!!
+        )
+
+        loadedStyle.addSource(GeoJsonSource("dropped-icon-image"))
+
+        val symbol = SymbolLayer(
+            DROPPED_MARKER_LAYER_ID, "dropped-icon-image"
+        )
+
+        symbol.withProperties(
+            PropertyFactory.iconImage("dropped-icon-image"),
+            PropertyFactory.visibility(Property.NONE),
+            PropertyFactory.iconAllowOverlap(true),
+            PropertyFactory.iconIgnorePlacement(true)
+        )
+
+        loadedStyle.addLayer(symbol)
     }
 }
