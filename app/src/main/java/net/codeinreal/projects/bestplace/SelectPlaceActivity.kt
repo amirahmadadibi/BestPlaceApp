71b4b5e447a12751e4fcc5b826fbce68ab935806
amirahmadadibi <amirahmadadibi@gmail.com>
2021-06-27 23:02:02 +0430

    E26-initiate marker

--- app/src/main/java/net/codeinreal/projects/bestplace/SelectPlaceActivity.kt
+++ app/src/main/java/net/codeinreal/projects/bestplace/SelectPlaceActivity.kt
@@ -1,14 +1,21 @@
 package net.codeinreal.projects.bestplace
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.Style
 import net.codeinreal.projects.bestplace.databinding.ActivitySelectPlaceBinding
 
+
 class SelectPlaceActivity : AppCompatActivity() {
     lateinit var binding: ActivitySelectPlaceBinding
+    private var hoveringMarker: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Mapbox.getInstance(
@@ -21,7 +28,18 @@ class SelectPlaceActivity : AppCompatActivity() {
         binding.mapView.onCreate(savedInstanceState)
         binding.mapView.getMapAsync { mapBoxMap ->
             mapBoxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+                hoveringMarker = ImageView(this@SelectPlaceActivity)
+                hoveringMarker!!.setBackgroundResource(R.drawable.ic_marker_red)
 
+                val imageLocation = FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    Gravity.CENTER
+                )
+
+                hoveringMarker!!.layoutParams = imageLocation
+
+                binding.mapView.addView(hoveringMarker)
             }
         }
     }
